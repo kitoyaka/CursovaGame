ccfd73b330512bca9c80c39bf584e253bc41290e
kitoyaka <r.igry3@gmail.com>
2024-01-28 20:35:46 +0200

    added black screen

--- android/src/main/kotlin/com/github/kitoyaka/CursovaGame/android/AndroidLauncher.kt
+++ android/src/main/kotlin/com/github/kitoyaka/CursovaGame/android/AndroidLauncher.kt
@@ -5,14 +5,49 @@ import android.os.Bundle
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 import com.github.kitoyaka.CursovaGame.CursovaGame
+import com.badlogic.gdx.Game
+import com.badlogic.gdx.Screen
 
+class MyEmptyScreen(private val game: Game) : Screen {
+    override fun show() {
+        // це метот який викликається коли є екран
+    }
+
+    override fun render(delta: Float) {
+        // метод який малює графіку (кадри render)
+    }
+
+    override fun resize(width: Int, height: Int) {
+        // це для зміни екрана
+    }
+
+    override fun pause() {
+        // Метод який викликається коли звертаєш додаток
+    }
+
+    override fun resume() {
+        // Метод який возобновляє коли вертаєшся в додаток
+    }
+
+    override fun hide() {
+        // Метод який викликається коли скриваєш екран
+    }
+
+    override fun dispose() {
+        // Метод для того аби виключити екран
+    }
+}
+class MyGame : Game() {
+    override fun create() {
+        // Инициализация вашей игры
+        setScreen(MyEmptyScreen(this))
+    }
+}
 /** Launches the Android application. */
 class AndroidLauncher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initialize(CursovaGame(), AndroidApplicationConfiguration().apply {
-            // Configure your application here.
-            useImmersiveMode = true // Recommended, but not required.
-        })
+        val config = AndroidApplicationConfiguration()
+        initialize(MyGame(), config)
     }
 }

33323ae143f79f7ccfef90fd2c8b1d1ed9d2405f
kitoyaka <r.igry3@gmail.com>
2024-01-28 22:44:01 +0200

    added black screen

--- android/src/main/kotlin/com/github/kitoyaka/CursovaGame/android/AndroidLauncher.kt
+++ android/src/main/kotlin/com/github/kitoyaka/CursovaGame/android/AndroidLauncher.kt
@@ -1,5 +1,6 @@
 package com.github.kitoyaka.CursovaGame.android
 
+import android.media.Image
 import android.os.Bundle
 
 import com.badlogic.gdx.backends.android.AndroidApplication
@@ -7,40 +8,34 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 import com.github.kitoyaka.CursovaGame.CursovaGame
 import com.badlogic.gdx.Game
 import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.utils.viewport.ExtendViewport
+import ktx.app.KtxScreen
+import ktx.graphics.use
+import ktx.log.logger
 
-class MyEmptyScreen(private val game: Game) : Screen {
+class GameScreen : KtxScreen {
+    private val spriteBatch: Batch = SpriteBatch()
+    private val texture: Texture = Texture("assets/BackScreen/MainMenu.jpg")
+
     override fun show() {
-        // це метот який викликається коли є екран
     }
-
     override fun render(delta: Float) {
-        // метод який малює графіку (кадри render)
+        spriteBatch.use { it.draw(texture,0f,0f) }
     }
-
     override fun resize(width: Int, height: Int) {
-        // це для зміни екрана
     }
-
-    override fun pause() {
-        // Метод який викликається коли звертаєш додаток
-    }
-
-    override fun resume() {
-        // Метод який возобновляє коли вертаєшся в додаток
-    }
-
-    override fun hide() {
-        // Метод який викликається коли скриваєш екран
-    }
-
     override fun dispose() {
-        // Метод для того аби виключити екран
     }
 }
+
 class MyGame : Game() {
     override fun create() {
-        // Инициализация вашей игры
-        setScreen(MyEmptyScreen(this))
+        // ініціалізація гри
+        setScreen(GameScreen())
     }
 }
 /** Launches the Android application. */
